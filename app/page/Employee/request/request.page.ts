9fdc046a1e3703f659a0b9442324a6c6492004a8
sayed-murtda <S.murtda@gmail.com>
2022-12-27 14:13:44 +0300

    add chancel and accept and Reject Request shift

--- app/page/Employee/request/request.page.ts
+++ app/page/Employee/request/request.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../../data.service';
 import { Emp, EmployeeService } from '../employee.service';
 
 @Component({
@@ -7,11 +8,25 @@ import { Emp, EmployeeService } from '../employee.service';
   styleUrls: ['./request.page.scss'],
 })
 export class RequestPage implements OnInit {
-    
-  constructor(public empServ:EmployeeService) { }
+  week=['Sanday','Moday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+  constructor(public empServ:EmployeeService,public dataSrv: DataService) { }
   
   ngOnInit() {      
     
+  }
+
+  cancel(id,day){
+    this.dataSrv.emp.StatusSwitch[day]=-1;
+    this.empServ.CancelRequests(this.dataSrv.emp.id, this.dataSrv.emp.StatusSwitch,id)
+  }
+  accept(id,day){
+    this.dataSrv.emp.StatusSwitch[day]=-1;
+    this.empServ.CancelRequests(this.dataSrv.emp.id, this.dataSrv.emp.StatusSwitch,id)
+  }
+  Reject(id,day){
+    this.dataSrv.emp.StatusSwitch[day]=-1;
+    this.empServ.CancelRequests(this.dataSrv.emp.id, this.dataSrv.emp.StatusSwitch,id)
   }
 
 }
