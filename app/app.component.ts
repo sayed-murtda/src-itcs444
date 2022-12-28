8ce55d8fd688de11607ab847ae997c6808a7e021
S3li98 <s.ali.uob98@gmail.com>
2022-12-28 09:38:11 +0300

    fix some errors

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,9 +9,9 @@ export class AppComponent {
   disaply=false;
   public appPages = [
     { title: 'Casher', content: [{ title: 'Casher', url: '/casher'} ]},
-    { title: 'Items', content: [{ title: 'Show', url: '/items' },{ title: 'edit', url: '/item/edit' },{ title: 'Remove', url: '/remove' } ]},
+    { title: 'Items', content: [{ title: 'Show', url: '/items' }]},
     { title: 'Employees', content: [{ title: 'switchShift', url: '/switch-shift' },{title:'request', url: '/request'} ]},
-    { title: 'Management', content: [{ title: 'ShowEmployee', url: '/employees' },{ title: 'addEmployee', url: '/add' },{ title: 'addsuppliers', url: '/add-sup' },{ title: 'addEmployee', url: '/add' },{ title: 'Request Supplier', url: 'suppliers/request' }, ]},
+    { title: 'Management', content: [{ title: 'ShowEmployee', url: '/employees' },{ title: 'addEmployee', url: '/add' },{ title: 'addsuppliers', url: '/add-sup' },{ title: 'Request Supplier', url: 'suppliers/request' }, ]},
     { title: 'suppliers', content: [{ title: 'Add-items', url: 'suppliers/add' },{ title: 'Show items', url: 'sup-items' },{ title: 'Show Suppliers', url: 'suppliers/show-sup' },{ title: 'Show-Requests', url: 'show-request' }]},
     { title: 'Reports', content: [{ title: 'Orders', url: 'order-report' }]},
   ]; 
